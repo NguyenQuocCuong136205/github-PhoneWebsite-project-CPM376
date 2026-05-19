387a8fedce2389c3fd87c88e6875119ad9b2aeef
Nguyen Quoc Cuong <nguyenquoccuongmusic@gmail.com>
2026-05-19 21:50:34 +0700

    submit project exercise day 2

---
+++ Lab2_PhoneShop/PhoneShopDB/PhoneShopDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Lab2_PhoneShop.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lab2_PhoneShop.PhoneShopDB
+{
+    public class PhoneShopDBContext : DbContext
+    {
+        public PhoneShopDBContext(DbContextOptions<PhoneShopDBContext> options) : base(options) { }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
